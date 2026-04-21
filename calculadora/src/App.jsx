49902f9f62fb339b37236d1943ba3284f308e014
Wadri02 <lumbi.2003.adrian@gmail.com>
2026-04-20 22:40:38 -0500

    feat: :art: home page with two buttons
    Home page with two buttons: frist button plus numbers and second button calculator

--- calculadora/src/App.jsx
+++ calculadora/src/App.jsx
@@ -1,11 +1,82 @@
-import Sumador from './Sumador';
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <Sumador />
-    </div>
-  );
-}
+    const [num1, setNum1] = useState('');
+    const [num2, setNum2] = useState('');
+    const [showPlus, setshowPlus] = useState(false);
+    const [showCalculator, setshowCalculator] = useState(false);
+    
+    const manejarSuma = () => {
+        const resultado = Number(num1) + Number(num2);
 
+        if (isNaN(resultado)){
+            alert("Ingresar numeros validos para realizar la suma")
+        }else{
+            alert(`La suma es: ${resultado}`);
+        }
+
+    };
+
+    const ClickbuttonPnumber = () =>  {
+        setshowPlus(true);
+    } ;
+
+    const Clickcalculator = () =>  {
+        setshowCalculator(true);
+    } ;
+
+    const Homebutton = () =>  {
+        setshowPlus(false);
+        setshowCalculator(false);
+    } ;
+    return(
+
+        <>
+         {!showPlus && !showCalculator && (
+            <>
+            <button type="button" onClick={ClickbuttonPnumber}>
+            Plus numbers
+            </button>
+            <button type="button" onClick={Clickcalculator}>
+            Calculator
+            </button> 
+            </>
+            )
+         }
+         
+         {showPlus && (
+            <>
+            <button type="button" onClick={Homebutton}>
+            Home
+            </button>
+            <h2>Sumador</h2>
+            <input type='number' value={num1} onChange={(e => setNum1(e.target.value))}></input>   
+            <br></br>
+            <input type='number' value={num2} onChange={(e => setNum2(e.target.value))}></input>   
+            <br></br>
+            <button onClick={manejarSuma}>Sumar</button>
+            </>
+            )
+         }
+         {showCalculator && (
+            <>
+            <button type="button" onClick={Homebutton}>
+            Home
+            </button>
+            <h2>Calculator</h2>
+             to do later......
+            <input type='number' value={num1} onChange={(e => setNum1(e.target.value))}></input>   
+            <br></br>
+            <input type='number' value={num2} onChange={(e => setNum2(e.target.value))}></input>   
+            <br></br>
+            <button onClick={manejarSuma}>Sumar</button>
+            </>
+            )
+         }
+         
+         
+        </>
+    );
+
+}
 export default App;
